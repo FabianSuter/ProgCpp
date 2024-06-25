b410bc8dbf79d7dc08771ed19073e3b495b234ff
FabianSuter <suter117@gmail.com>
2024-06-25 16:23:44 +0200

    Anpassungen von Steiner, Vers 1.1.6 eingefügt, einzelne Änderungen

--- code/forEach.cpp
+++ code/forEach.cpp
@@ -1,3 +1,5 @@
 for(<DataType> <IteratorName> : arrayName){
     ;//Loop where <IteratorName> is each element
 }
+// <DataType> kann als 'auto' gesetzt werden
+// -> erkennt Typ des Arrays automatisch
